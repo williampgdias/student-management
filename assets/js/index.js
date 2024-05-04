9f4df93b2f887255d6a6bd4bcd4d5966ba7c107b
William Dias <smdwilliam89@gmail.com>
2024-05-04 05:53:11 -0300

    fix(index.js): Make the code easier and reuse the functions.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,43 +1,59 @@
 $(document).ready(function () {
     const inputs = $('.form-floating');
 
-    let emailError = $('.error-message-email');
-    let passwordError = $('.error-message-password');
-    let nameError = $('.error-message-name');
+    function errorsMessage(message, targetDiv) {
+        $(targetDiv).text(message).show();
+    }
 
-    emailError.hide();
-    passwordError.hide();
-    nameError.hide();
+    errorsMessage('', '.error-message-email');
+    errorsMessage('', '.error-message-password');
+    errorsMessage('', '.error-message-name');
+
+    $('#floatingEmail').keyup(function () {
+        let specialChar = '@';
+        let emailValue = $(this).val();
+
+        if (emailValue.indexOf(specialChar) !== -1) {
+            errorsMessage('', '.error-message-email');
+        } else {
+            errorsMessage(
+                'Please, enter a valid email address containing "@"',
+                '.error-message-email'
+            );
+        }
+    });
+
+    $('#floatingInput').keyup(function () {
+        errorsMessage('', '.error-message-name');
+    });
+
+    $('#floatingPassword').keyup(function () {
+        errorsMessage('', '.error-message-password');
+    });
 
     $('.needs-validation').submit(function (event) {
         // Condition to Email Error
         if ($('#floatingEmail').val() == '') {
-            emailError.show();
+            errorsMessage(
+                'Please, enter a valid email',
+                '.error-message-email'
+            );
             event.preventDefault();
         }
 
         // Condition to Name Error
         if ($('#floatingInput').val() == '') {
-            nameError.show();
+            errorsMessage('Please, enter a valid name', '.error-message-name');
             event.preventDefault();
         }
 
         // Condition to Password Error
         if ($('#floatingPassword').val() == '') {
-            passwordError.show();
+            errorsMessage(
+                'Please, enter a valid password',
+                '.error-message-password'
+            );
             event.preventDefault();
         }
-
-        $('#floatingEmail').keyup(function () {
-            emailError.hide();
-        });
-
-        $('#floatingInput').keyup(function () {
-            nameError.hide();
-        });
-
-        $('#floatingPassword').keyup(function () {
-            passwordError.hide();
-        });
     });
 });
