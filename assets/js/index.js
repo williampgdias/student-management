1ed1e1c286c7d661ae586bc5da2a3c7719564d1e
William Dias <smdwilliam89@gmail.com>
2024-05-06 06:22:52 -0300

    feat(index.js): Add three functions. 1. validateInputFieldOnKeyUp, 2. addNewStudent, 3. renderStudents.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,95 @@
 $(document).ready(function () {
-    const inputs = $('.form-floating');
+    let students = [];
 
     function errorsMessage(message, targetDiv) {
         $(targetDiv).text(message).show();
     }
 
+    function validateInputFieldsOnKeyUp() {
+        $('#floatingEmail').keyup(function () {
+            let specialChar = '@';
+            let emailValue = $(this).val();
+
+            if (emailValue.indexOf(specialChar) !== -1) {
+                errorsMessage('', '.error-message-email');
+            } else {
+                errorsMessage(
+                    'Please, enter a valid email address containing "@"',
+                    '.error-message-email'
+                );
+            }
+        });
+
+        $('#floatingInput').keyup(function () {
+            errorsMessage('', '.error-message-name');
+        });
+
+        $('#floatingPassword').keyup(function () {
+            errorsMessage('', '.error-message-password');
+        });
+    }
+
+    function addNewStudent() {
+        let studentID = $('#floatingId').val();
+        let studentName = $('#floatingName').val();
+        let studentEmail = $('#floatingEmail').val();
+        let studentPpsNumber = $('#floatingPpsNumber').val();
+
+        let newStudent = {
+            id: studentID,
+            name: studentName,
+            email: studentEmail,
+            ppsNumber: studentPpsNumber,
+        };
+
+        students.push(newStudent);
+
+        students.sort(function (a, b) {
+            return a.id - b.id;
+        });
+
+        renderStudents();
+
+        $('#floatingId').val('');
+        $('#floatingName').val('');
+        $('#floatingEmail').val('');
+        $('#floatingPpsNumber').val('');
+    }
+
+    function renderStudents() {
+        const cardStudents = $('.table').children('tbody');
+
+        cardStudents.empty();
+
+        students.forEach(function (student) {
+            const studentsIdFormatted =
+                student.id.length <= 1
+                    ? '00' + student.id
+                    : student.id.length <= 2
+                    ? '0' + student.id
+                    : student.id;
+
+            const html = `
+                <tr class="table-hover">
+                    <th scope="row">${studentsIdFormatted}</th>
+                    <td>${student.name}</td>
+                    <td>${student.email}</td>
+                    <td>${student.ppsNumber}</td>
+                    <td>
+                        <span class="content-delete-button">x</span>
+                    </td>
+                </tr>
+            `;
+
+            cardStudents.append(html);
+        });
+    }
+
+    validateInputFieldsOnKeyUp();
+
     errorsMessage('', '.error-message-email');
     errorsMessage('', '.error-message-password');
     errorsMessage('', '.error-message-name');
-
-    $('#floatingEmail').keyup(function () {
-        let specialChar = '@';
-        let emailValue = $(this).val();
-
-        if (emailValue.indexOf(specialChar) !== -1) {
-            errorsMessage('', '.error-message-email');
-        } else {
-            errorsMessage(
-                'Please, enter a valid email address containing "@"',
-                '.error-message-email'
-            );
-        }
-    });
-
-    $('#floatingInput').keyup(function () {
-        errorsMessage('', '.error-message-name');
-    });
-
-    $('#floatingPassword').keyup(function () {
-        errorsMessage('', '.error-message-password');
-    });
 
     $('.needs-validation').submit(function (event) {
         // Condition to Email Error
@@ -55,5 +115,13 @@ $(document).ready(function () {
             );
             event.preventDefault();
         }
+    });
+
+    $('#addStudentsButton').click(function (event) {
+        event.preventDefault();
+
+        addNewStudent();
+
+        $('#floatingId').text('');
     });
 });
